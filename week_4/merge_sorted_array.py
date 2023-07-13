117fbd51a3181125ef5acbef6352888bedc6c23b
Haqq Bassit <munagahmnh@gmail.com>
2023-07-13 21:04:32 +0000

    add merge sorted array

--- week_4/merge_sorted_array.py
+++ week_4/merge_sorted_array.py
@@ -1,11 +1,17 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = 0
-        while i < m+n:
-            if nums1[i] == 0:
-                if len(nums2) != 0:
-                    nums1.remove(nums1[i])
-                    nums1.insert(i,nums2[0])
-                    nums2.pop(0)
-            i+=1
-        nums1.sort()
+        final=[]
+        pointer1=pointer2=0
+        while pointer1 < m and pointer2 < n:
+            if nums1[pointer1] <= nums2[pointer2]:
+                final.append(nums1[pointer1])
+                pointer1+=1
+            elif nums2[pointer2] < nums1[pointer1]:
+                final.append(nums2[pointer2])
+                pointer2+=1
+
+        if pointer1>=m:
+            final.extend(nums2[pointer2:n])
+        if pointer2>=n:
+            final.extend(nums1[pointer1:m])
+        nums1[:]=final[:]
