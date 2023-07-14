187b98eeba9b13f4c1da7dc15cd950a6dd69caa6
Haqq Bassit <munagahmnh@gmail.com>
2023-07-14 20:08:37 +0000

    add boats to save people

--- week_4/boats_to_save_people.py
+++ week_4/boats_to_save_people.py
@@ -8,16 +8,11 @@ class Solution:
         pairs=0
         print(people)
         while left < right:
-            if people[left]+people[right]==limit:
+            if people[left]+people[right]<=limit:
                 pairs+=2
                 count+=1
                 left+=1
                 right-=1
-            elif people[left]+people[right]<limit:
-                right-=1
-                left+=1
-                pairs+=2
-                count+=1
             else:
                 right-=1
         count+=len(people)-pairs
