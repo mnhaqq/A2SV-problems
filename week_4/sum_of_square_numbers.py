d2f777fa12eca7a0338eb98b00ab0f86f22816f9
Haqq Bassit <munagahmnh@gmail.com>
2023-07-16 08:40:37 +0000

    add sum of sqaure numbers

---
+++ week_4/sum_of_square_numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        arr = [i for i in range(int(sqrt(c))+1)]
+        left=0
+        right=len(arr)-1
+        while left<=right:
+            if arr[left]**2 + arr[right]**2 == c:
+                return True
+            elif arr[left]**2 + arr[right]**2 < c:
+                left+=1
+            else:
+                right-=1
+        return False
