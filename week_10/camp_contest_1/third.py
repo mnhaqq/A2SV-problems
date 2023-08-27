6429d319e43be43e315ceef51b2992759825310f
Haqq Bassit <munagahmnh@gmail.com>
2023-08-27 09:00:59 +0000

    optimize sub-optimal code

--- week_10/camp_contest_1/third.py
+++ week_10/camp_contest_1/third.py
@@ -6,13 +6,13 @@ def solve():
  
     sorted_v = sorted(v)
     sum_v = sum(v)
-    pr_sum_v = []
+    pr_sum_v = [0]
     pr_sum = 0
     for i in range(n):
         pr_sum+=v[i]
         pr_sum_v.append(pr_sum)
 
-    pr_sum_sorted_v=[]
+    pr_sum_sorted_v=[0]
     pr_sum = 0
     for i in range(n):
         pr_sum+=sorted_v[i]
@@ -21,17 +21,13 @@ def solve():
     answer = []
     for i in range(m):
         q_type = questions[i][0]
-        l = questions[i][1]-1
-        r = questions[i][2]-1
+        l = questions[i][1]
+        r = questions[i][2]
 
         if q_type == 1:
-            ans = pr_sum_v[r]
-            if l-1 >= 0:
-                ans -= pr_sum_v[l-1]
+            ans = pr_sum_v[r]-pr_sum_v[l-1]
         else:
-            ans = pr_sum_sorted_v[r]
-            if l-1 >= 0:
-                ans -= pr_sum_sorted_v[l-1]
+            ans = pr_sum_sorted_v[r]-pr_sum_sorted_v[l-1]
         print(ans)     
  
 solve()
