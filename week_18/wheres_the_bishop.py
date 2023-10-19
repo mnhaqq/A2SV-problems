3a8481a2f28eecabe69611de0dc440e73e095d0d
Haqq Bassit <munagahmnh@gmail.com>
2023-10-19 21:50:55 +0000

    add solution

--- week_18/wheres_the_bishop.py
+++ week_18/wheres_the_bishop.py
@@ -4,8 +4,12 @@ def solve():
         row = list(input())
         if i != 0:
             matrix.append(row)
-    for row in matrix:
-        print(row)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])-2):
+            if matrix[i][j] == matrix[i][j+2] == "#":
+                ans = [i+2, j+2]
+                print(*ans)
+                return  
 
 
 t = int(input())
