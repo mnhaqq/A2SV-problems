9be9879b385ddebc8bf3987b7c3af1cc75b2c33c
Haqq Bassit <munagahmnh@gmail.com>
2023-11-07 21:13:13 +0000

    add solution

---
+++ week_21/same_tree.py
@@ -0,0 +1,16 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if (not p or not q) or (p.val != q.val):
+            return False
+        first = self.isSameTree(p.left, q.left)
+        second = self.isSameTree(p.right, q.right)
+        return first and second
