c730e8bb4cc45704c7a4af1923b0d398e9ec0174
Haqq Bassit <munagahmnh@gmail.com>
2023-11-08 20:39:34 +0000

    add solution

---
+++ week_21/search_in_binary_search_tree.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        elif root.val == val:
+            return root
+        elif root.val > val:
+            ans = self.searchBST(root.left, val)
+        else:
+            ans = self.searchBST(root.right, val)
+        return ans
