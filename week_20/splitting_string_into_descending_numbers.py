245a396435b9bde4545afdfb845b5daa60cc4dd6
Haqq Bassit <munagahmnh@gmail.com>
2023-11-19 09:20:43 +0000

    refactor solution

--- week_20/splitting_string_into_descending_numbers.py
+++ week_20/splitting_string_into_descending_numbers.py
@@ -1,28 +1,20 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-        def is_valid_state(prev_num, idx):
-            return idx == len(s)
-
-        def get_candidates(prev_num, idx):
-            arr = []
+        def backtrack(prev_num, idx):
+            nonlocal check
+            if idx == len(s):
+                check = True
+                return
             for i in range(idx, len(s)):
                 if idx == 0 and i == len(s) - 1:
                     break
                 if idx == 0:
-                    arr.append((int(s[:i+1]), i+1))
+                    backtrack(int(s[idx:i+1]), i+1)
                 elif int(s[idx:i+1]) == prev_num-1:
-                    arr.append((int(s[idx:i+1]), i+1))
-            return arr
+                    backtrack(int(s[idx:i+1]), i+1)
+                else:
+                    continue
 
-        def search(prev_num, idx):
-            nonlocal solutions
-            if is_valid_state(prev_num, idx):
-                solutions = True
-                return
-
-            for prev, i in get_candidates(prev_num, idx):
-                search(prev, i)
-
-        solutions = False
-        search(-1, 0)       
-        return solutions      
+        check = False
+        backtrack(-1, 0)
+        return check
