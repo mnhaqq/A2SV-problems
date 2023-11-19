da216e06a85071471d0cf3af091fa075571b552f
Haqq Bassit <munagahmnh@gmail.com>
2023-11-19 01:18:13 +0000

    refactor solution

--- week_20/permutations.py
+++ week_20/permutations.py
@@ -1,25 +1,16 @@
 class Solution:
     def permute(self, nums: list[int]) -> list[list[int]]:
-        def is_valid_state(state):
-            return len(state) == len(nums)
-
-        def get_candidates(state):
-            global_state = []
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                result.append(curr.copy())
+                return
+            
             for i in range(len(nums)):
-                if nums[i] not in state:
-                    global_state.append(nums[i])
-            return global_state
-
-        def search(state, solutions):
-            if is_valid_state(state):
-                solutions.append(state.copy())
-
-            for candidate in get_candidates(state):
-                state.append(candidate)
-                search(state, solutions)
-                state.pop()
-
-        solutions = []
-        state = []
-        search(state, solutions)
-        return solutions
+                if nums[i] not in curr:
+                    curr.append(nums[i])
+                    backtrack(curr)
+                    curr.pop()
+                    
+        result = []
+        backtrack([])
+        return result
