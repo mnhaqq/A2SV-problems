e2590e8e4c0f11b5dfd545c2ab0274def495ba42
Haqq Bassit <munagahmnh@gmail.com>
2023-11-19 00:36:13 +0000

    refactor solution

--- week_19/combinations.py
+++ week_19/combinations.py
@@ -1,26 +1,20 @@
 class Solution:
     def combine(self, n: int, k: int) -> list[list[int]]:
-        def is_valid_state(state):
-            return len(state) == k
-
-        def get_candidates(state):
-            if len(state) == 0:
-                global_state = set([i for i in range(1, n+1)])
+        def backtrack(curr):
+            if len(curr) == k:
+                result.append(curr[:])
+                return
+            if len(curr) == 0:
+                for i in range(1, n+1):
+                    curr.append(i)
+                    backtrack(curr)
+                    curr.pop()
             else:
-                global_state = set([i for i in range(max(list(state)), n+1)])
-                
-            return global_state.difference(state)
-
-        def search(state, solutions):
-            if is_valid_state(state):
-                solutions.append(state.copy())
-
-            for candidate in get_candidates(state):
-                state.add(candidate)
-                search(state, solutions)
-                state.remove(candidate)
-
-        solutions = []
-        state = set()
-        search(state, solutions)
-        return solutions
+                for i in range(max(curr)+1, n+1):
+                    curr.append(i)
+                    backtrack(curr)
+                    curr.pop()
+        
+        result = []
+        backtrack([])
+        return result
