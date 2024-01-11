3243e316b8625d2f4b66c6a62b0c1863d41139c6
Haqq Bassit <munagahmnh@gmail.com>
2024-01-11 12:38:13 +0000

    add solution

---
+++ week_30/find_all_numbers_disappeared_in_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        
+        res = []
+        i = 0
+        while i < n:
+            if nums[i] != (i + 1) and nums[i] != nums[nums[i]-1]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+            else:
+                i += 1
+
+        for i in range(len(nums)):
+            if nums[i] != (i+1):
+                res.append(i+1)
+        return res
