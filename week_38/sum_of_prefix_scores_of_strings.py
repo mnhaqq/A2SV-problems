5d655389dca958c1bfcedc04ae50161c4e76c62c
mnhaqq <118978306+mnhaqq@users.noreply.github.com>
2024-03-04 12:02:12 +0000

    Update sum_of_prefix_scores_of_strings.py

--- week_38/sum_of_prefix_scores_of_strings.py
+++ week_38/sum_of_prefix_scores_of_strings.py
@@ -8,21 +8,17 @@ class Solution:
             if c not in curr:
                 curr[c] = {}
 
+            curr = curr[c]
             if "count" not in curr:
                 curr["count"] = 1
             else:
                 curr["count"] += 1
-            curr = curr[c]
-        if "count" not in curr:
-            curr["count"] = 1
-        else:
-            curr["count"] += 1
         curr["#"] = {}
 
     def calc_score(self, word):
-        curr = self.root
         score = 0
 
+        curr = self.root
         for c in word:
             curr = curr[c]
             if "count" in curr:
@@ -32,11 +28,8 @@ class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         for word in words:
             self.add_word(word)
-
-        del self.root['count']
         
         ans = [0] * len(words)
         for i in range(len(words)):
             ans[i] = self.calc_score(words[i])
-        
         return ans
