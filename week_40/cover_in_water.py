6e1791c8c84f4749e8bc21c6540c4647966d9d4c
mnhaqq <118978306+mnhaqq@users.noreply.github.com>
2024-03-19 09:58:40 +0000

    Create cover_in_water.py

---
+++ week_40/cover_in_water.py
@@ -0,0 +1,13 @@
+def solve():
+    n = int(input())
+    s = "#" + input() + "#"
+ 
+    for i in range(n):
+        if s[i] == s[i-1] == s[i+1] == ".":
+            print(2)
+            return
+    print(s.count("."))
+ 
+t = int(input())
+for _ in range(t):
+    solve()
