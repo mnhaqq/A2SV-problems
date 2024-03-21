56ee5278d2a64a4aac7e926201bc7693592e12e3
mnhaqq <118978306+mnhaqq@users.noreply.github.com>
2024-03-21 15:21:43 +0000

    Update index_of_first_occurrence_in_string.py

--- week_40/index_of_first_occurrence_in_string.py
+++ week_40/index_of_first_occurrence_in_string.py
@@ -1,24 +1,32 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        mod = 10 ** 9 + 7
         def hash(word):
             ans = 0
             n = len(word)
+            a = 31 ** (n-1)
             for i in range(n):
-                ans += (ord(word[i]) - ord('a') + 1) * (31 ** (n-i-1))
+                ans += (ord(word[i]) - ord('a') + 1) * (31 ** (n-1-i))
+                ans %= mod
+
             return ans
         
         h_needle = hash(needle)
         window = hash(haystack[:len(needle)])
         n = len(needle)
+        p = 31 ** n
 
         l = 0
         for r in range(n, len(haystack)):
             if h_needle == window:
-                return l % (10 ** 9 + 7)
-            window *= 31
-            window -= (hash(haystack[l]) * (31 ** n))
-            window += ord(haystack[r]) - ord('a') + 1
+                return l
+            window *= 31 
+            window = window % mod
+            window -= (hash(haystack[l]) * p)
+            window += (ord(haystack[r]) - ord('a') + 1)
+            window = window % mod
             l += 1
+
         if h_needle == window:
-            return l % (10 ** 9 + 7)
+            return l
         return -1
